aa95ba521ca8eb7f0155891eb5bcbd9182bc3382
DanieleKun <pucciodaniele95@gmail.com>
2022-07-14 16:08:36 +0200

    add movies list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,9 @@
 @extends('templates.base')
 
+@section('pageMain')
+    <main>
+        @foreach ($movies as $movie)
+            <li>{{$movie->title}}</li>
+        @endforeach
+    </main>
+@endsection
--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -8,5 +8,7 @@
 </head>
 <body>
     
+
+    @yield('pageMain')
 </body>
 </html>
